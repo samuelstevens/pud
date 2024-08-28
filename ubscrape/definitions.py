293c0353e3c901a69194f1a5145920f1ec1a25d3
Vidura Prasangana <vidura.prasangana16@gmail.com>
2024-08-28 21:13:26 +0530

    increase pool count

--- ubscrape/definitions.py
+++ ubscrape/definitions.py
@@ -78,7 +78,7 @@ def write_definition_by_api(word_t: Tuple[str]) -> List[dict]:
 
 
 def define_all_words():
-    pool = mp.Pool(50)
+    pool = mp.Pool(10)
 
     words = CON.execute(
         'SELECT word FROM word WHERE complete = 0').fetchall()
